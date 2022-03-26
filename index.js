c8dbd556703010f6eb67668223bd901dabf033d9
MUSSAFARA-RIAZ <85123767+MUSSAFARA-RIAZ@users.noreply.github.com>
2022-03-27 00:18:41 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+ function func(){
+         document.getElementById("section").style.display='none';
+        if(document.getElementById("myitems").style.display=='none'){
+                document.getElementById("myitems").style.display='block';
+                
+        }
+ }
